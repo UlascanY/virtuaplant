6df96232ba4d62275d49ebb6d6a90a69b8bd435d
Paradoxxs <Aston.dk@gmail.com>
2022-09-14 09:58:07 +0200

    Update start.sh

--- plants/oil-refinery/start.sh
+++ plants/oil-refinery/start.sh
@@ -3,6 +3,9 @@
 echo "VirtuaPlant -- Bottle-filling Factory"
 echo "- Starting World View"
 #./oil_world.py -t localhost &
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
 python3 ./oil_world_noGPIO.py -t 0.0.0.0 &
 echo "- Starting HMI"
 #./oil_hmi.py -t localhost &
