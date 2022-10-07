47b1391443d0cb20797e8b776371f77528dffed9
UlascanY <Ulascan.Yildirim@hotmail.com>
2022-10-07 18:34:49 +0200

    bottle_fillig attacks fix

--- plants/bottle-filling/attacks/never_stop.py
+++ plants/bottle-filling/attacks/never_stop.py
@@ -11,7 +11,14 @@ log.setLevel(logging.INFO)
 #####################################
 # Code
 #####################################
-client = ModbusClient('localhost', port=5020)
+def get_ip():
+    from netifaces import interfaces, ifaddresses, AF_INET
+    for ifaceName in interfaces():
+        addresses = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr':'No IP addr'}] )]
+        if ifaceName == "enp0s3":
+            return ''.join(addresses)
+
+client = ModbusClient(str(get_ip()), port=5020)
 
 try:
     client.connect()
@@ -24,4 +31,4 @@ try:
 except KeyboardInterrupt:
     client.close()
 except ConnectionException:
-    print "Unable to connect / Connection lost"
+    print("Unable to connect / Connection lost")
