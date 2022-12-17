3e212c2dff0c31f133ef7c885722cf2597eb5813
UlascanY <Ulascan.Yildirim@hotmail.com>
2022-12-17 17:32:24 +0100

    requiements fix

--- plants/oil-refinery/start.sh
+++ plants/oil-refinery/start.sh
@@ -2,9 +2,9 @@
 
 echo "VirtuaPlant -- Bottle-filling Factory"
 echo "- Starting World View"
-./oil_world.py -t localhost &
+./dist/oil_world -t localhost &
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
 echo "- Starting HMI"
-./oil_hmi.py -t localhost &
+./dist/oil_hmi -t localhost &
